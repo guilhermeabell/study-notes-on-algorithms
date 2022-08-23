604684f5bde272a8da2c4862934a33e3543337e7
vulgoguix <gpeuvis@gmail.com>
2022-08-23 09:55:48 -0300

    added: first solution contains duplicate

---
+++ 01-containsDuplicate.js
@@ -0,0 +1,31 @@
+// description 
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+
+// solution: 
+
+var containsDuplicate = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+      if (nums.indexOf(nums[i]) !== nums.lastIndexOf(nums[i])) {
+        return true
+      }
+    }
+    return false
+  };
+
